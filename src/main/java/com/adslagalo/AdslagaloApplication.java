56da104fc193cd43ccc4b30940ec60519699a304
Shubham Kumar <skbeg1999@gmail.com>
2026-02-16 15:38:48 +0530

    Initial backend setup

---
+++ src/main/java/com/adslagalo/AdslagaloApplication.java
@@ -0,0 +1,13 @@
+package com.adslagalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdslagaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdslagaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adslagalo/AdslagaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adslagalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdslagaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
